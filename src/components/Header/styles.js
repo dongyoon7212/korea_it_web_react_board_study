24b75e8d5e27238101b0abba896ea72ac8571bcd
dongyoon7212 <dongyoon7212@naver.com>
2025-08-20 20:16:41 +0900

    Init: board

---
+++ src/components/Header/styles.js
@@ -0,0 +1,9 @@
+import { css } from "@emotion/react";
+
+export const header = css`
+  height: 60px;
+  width: 100%;
+  display: flex;
+  border-bottom: 1px solid #dbdbdb;
+  box-sizing: border-box;
+`
