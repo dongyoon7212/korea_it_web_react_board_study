2f96132abd803ba74713d90cfae627549636d2ed
dongyoon7212 <dongyoon7212@naver.com>
2025-08-22 20:39:06 +0900

    Feat: react

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -2,6 +2,8 @@
 import * as s from "./styles";
 import AuthInput from "../../components/AuthInput/AuthInput";
 import { useEffect, useState } from "react";
+import { signupRequest } from "../../apis/auth/authApis";
+import { useNavigate } from "react-router-dom";
 
 function Signup() {
 	const [username, setUsername] = useState("");
@@ -9,6 +11,7 @@ function Signup() {
 	const [confirmPassword, setConfirmPassword] = useState("");
 	const [email, setEmail] = useState("");
 	const [errorMessage, setErrorMessage] = useState({});
+	const navigate = useNavigate();
 
 	const signupOnClickHandler = () => {
 		if (
@@ -27,6 +30,25 @@ function Signup() {
 		}
 
 		//회원가입 요청 API
+		signupRequest({
+			username: username,
+			password: password,
+			email: email,
+		})
+			.then((response) => {
+				console.log(response.data);
+				if (response.data.status === "success") {
+					alert(response.data.message);
+					navigate("/auth/signin");
+				} else if (response.data.status === "failed") {
+					alert(response.data.message);
+					return;
+				}
+			})
+			.catch((error) => {
+				alert("문제가 발생했습니다. 다시 시도해주세요.");
+				return;
+			});
 	};
 
 	useEffect(() => {
