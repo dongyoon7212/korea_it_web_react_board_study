b476ff78c6aa282bfcba8977dee49b37390c28ca
dongyoon7212 <dongyoon7212@naver.com>
2025-08-26 20:52:32 +0900

    Feat: react

--- src/pages/Write/Write.jsx
+++ src/pages/Write/Write.jsx
@@ -1,5 +1,16 @@
+/** @jsxImportSource @emotion/react */
+import * as s from "./styles";
+
 function Write() {
-	return <div>Write</div>;
+	return (
+		<div css={s.container}>
+			<input type="text" />
+			<textarea name="" id=""></textarea>
+			<div>
+				<button>게시하기</button>
+			</div>
+		</div>
+	);
 }
 
 export default Write;
---
+++ src/pages/Write/styles.js
@@ -0,0 +1,20 @@
+import { css } from "@emotion/react";
+
+export const container = css`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	width: 100%;
+	padding: 40px 200px;
+	box-sizing: border-box;
+	background-color: beige;
+
+	& > input {
+		border: 1px solid #dbdbdb;
+		background-color: transparent;
+		border-radius: 15px;
+		padding: 15px;
+		width: 100%;
+		outline: none;
+	}
+`;
