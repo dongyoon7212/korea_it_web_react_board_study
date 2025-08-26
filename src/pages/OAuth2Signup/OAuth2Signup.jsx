206d9f62fe9d0a23b5852b9b1d723a417619593f
dongyoon7212 <dongyoon7212@naver.com>
2025-08-26 19:34:44 +0900

    feat: react

--- src/pages/OAuth2Signup/OAuth2Signup.jsx
+++ src/pages/OAuth2Signup/OAuth2Signup.jsx
@@ -1,12 +1,51 @@
 /** @jsxImportSource @emotion/react */
+import { useEffect, useState } from "react";
+import AuthInput from "../../components/AuthInput/AuthInput";
 import * as s from "./styles";
+import { useSearchParams } from "react-router-dom";
 
 function OAuth2Signup() {
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+	const [confirmPassword, setConfirmPassword] = useState("");
+	const [email, setEmail] = useState("");
+	const [errorMessage, setErrorMessage] = useState({});
+	const [searchParam] = useSearchParams();
+
+	useEffect(() => {
+		setEmail(searchParam.get("email"));
+	}, [searchParam]);
+
 	return (
 		<div css={s.container}>
 			<h1>회원가입</h1>
 			<div css={s.box}>
-				<div css={s.inputBox}></div>
+				<div css={s.inputBox}>
+					<AuthInput
+						type={"text"}
+						placeholder={"아이디"}
+						state={username}
+						setState={setUsername}
+					/>
+					<AuthInput
+						type={"password"}
+						placeholder={"비밀번호"}
+						state={password}
+						setState={setPassword}
+					/>
+					<AuthInput
+						type={"password"}
+						placeholder={"비밀번호 확인"}
+						state={confirmPassword}
+						setState={setConfirmPassword}
+					/>
+					<AuthInput
+						type={"email"}
+						placeholder={"이메일"}
+						state={email}
+						setState={setEmail}
+					/>
+				</div>
 			</div>
 		</div>
 	);
