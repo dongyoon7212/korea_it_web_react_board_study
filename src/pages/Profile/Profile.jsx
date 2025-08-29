b8f24a2e8c1381b0f349c036164be8e4d132d3b4
dongyoon7212 <dongyoon7212@naver.com>
2025-08-29 21:46:52 +0900

    Feat: profile

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,8 +1,29 @@
 /** @jsxImportSource @emotion/react */
 import * as s from "./styles";
+import profileImg from "../../assets/profileImg.jpg";
 
 function Profile() {
-	return <div>Profile</div>;
+	return (
+		<div css={s.container}>
+			<div css={s.profileContainer}>
+				<div css={s.profileHeader}>
+					<div css={s.profileImgBox}>
+						<div>
+							<img src={profileImg} alt="profileImage" />
+						</div>
+					</div>
+					<div css={s.profileInfoBox}>
+						<h3>username</h3>
+						<div>
+							<p>email@naver.com</p>
+							<button>인증하기</button>
+						</div>
+					</div>
+				</div>
+				<div css={s.profileMain}></div>
+			</div>
+		</div>
+	);
 }
 
 export default Profile;
